1e101b139d1753969ad4395f2abb3d9d140539cc
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-07 20:14:12 +0100

    Improved test with "andExpect(jsonPath...)"

--- src/test/java/dev/jugapi/workcount/infrastructure/adapter/in/web/WorkRegistrationWebControllerTest.java
+++ src/test/java/dev/jugapi/workcount/infrastructure/adapter/in/web/WorkRegistrationWebControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.jugapi.workcount.application.port.in.CalculateMonthlyBalanceUseCase;
 import dev.jugapi.workcount.application.port.in.FindByMonthUseCase;
 import dev.jugapi.workcount.application.port.in.RegisterWorkDayUseCase;
+import dev.jugapi.workcount.domain.model.WorkRegistration;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,12 +14,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(WorkRegistrationWebController.class)
@@ -43,17 +47,21 @@ public class WorkRegistrationWebControllerTest {
     @Test
     @DisplayName("Should return 201 Created")
     void shouldReturn201WhenRegistrationIsValid() throws Exception {
-        WorkRegistrationWebRequest request = new WorkRegistrationWebRequest(
+        WorkRegistration request = new WorkRegistration(
                 LocalDate.of(2026, 3, 3),
                 LocalTime.of(8, 0),
-                LocalTime.of(16,0),
-                null
+                LocalTime.of(16, 0),
+                null,
+                Duration.ofHours(8)
         );
+
+        when(registerWorkDayUseCase.registerWorkDay(any())).thenReturn(request);
 
         mockMvc.perform(post("/api/work-registrations")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isCreated());
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.workingDay").value("2026-03-03"));
 
         verify(registerWorkDayUseCase).registerWorkDay(any());
     }
