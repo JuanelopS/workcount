bde641b1d629521b02f6fa8aa0a9c716ebfdc189
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-14 18:01:52 +0100

    Fix: rename InexistentRegisteredDay class to InexistentRegisteredDayException

--- src/main/java/dev/jugapi/workcount/domain/exception/InexistentRegisteredDayException.java
+++ src/main/java/dev/jugapi/workcount/domain/exception/InexistentRegisteredDayException.java
@@ -3,8 +3,8 @@ package dev.jugapi.workcount.domain.exception;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-public class InexistentRegisteredDay extends RuntimeException {
-    public InexistentRegisteredDay(LocalDate date) {
+public class InexistentRegisteredDayException extends RuntimeException {
+    public InexistentRegisteredDayException(LocalDate date) {
         super("No existen registros para el día " +
                 date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
     }
