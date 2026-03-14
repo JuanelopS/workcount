d377cb371cfe42d51471d6d48fdb1221f000cd4b
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-14 18:52:52 +0100

    feat: implement validation and initialization logic for WorkMonth record

--- src/main/java/dev/jugapi/workcount/domain/model/WorkMonth.java
+++ src/main/java/dev/jugapi/workcount/domain/model/WorkMonth.java
@@ -9,6 +9,19 @@ public record WorkMonth(
         List<WorkRegistration> registrations,
         Duration targetHours) {
 
+    public WorkMonth {
+        registrations = (registrations != null) ? List.copyOf(registrations) : List.of();
+
+        boolean checkList = registrations.stream()
+                .anyMatch(wr -> !YearMonth.from(wr.getWorkingDay()).equals(month));
+        if (checkList) {
+            throw new IllegalArgumentException("All registration must belong to the month " + month);
+        }
+
+        targetHours = (targetHours == null || targetHours.isNegative()) ? Duration.ZERO
+                : targetHours;
+    }
+
     public Duration calculateTotalHoursWorked() {
         return registrations.stream()
                 .map(WorkRegistration::getValidatedHours)
