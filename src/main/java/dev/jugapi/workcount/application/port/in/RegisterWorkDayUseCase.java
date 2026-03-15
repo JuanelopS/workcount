dbd9d4f75fc9a0284c53c0f7a0c15c7dfba9fb34
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-15 11:55:53 +0100

    Refactor: Externalize database credentials to environment variables in docker-compose.yml, add .env-default and update .gitignore

--- src/main/java/dev/jugapi/workcount/application/port/in/RegisterWorkDayUseCase.java
+++ src/main/java/dev/jugapi/workcount/application/port/in/RegisterWorkDayUseCase.java
@@ -3,5 +3,5 @@ package dev.jugapi.workcount.application.port.in;
 import dev.jugapi.workcount.domain.model.WorkRegistration;
 
 public interface RegisterWorkDayUseCase {
-    WorkRegistration registerWorkDay(WorkRegistration workRegistration);
+    WorkRegistration registerWorkDay(WorkRegistration wr);
 }
