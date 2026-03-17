6d471788993842c785df78404d0f7d4d37032f08
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-17 19:16:09 +0100

    Change string

--- src/main/java/dev/jugapi/workcount/domain/exception/InexistentWorkDayException.java
+++ src/main/java/dev/jugapi/workcount/domain/exception/InexistentWorkDayException.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class InexistentWorkDayException extends RuntimeException {
     public InexistentWorkDayException(LocalDate date) {
-        super("No existen registros para el día " +
+        super("No existen registros el día " +
                 date.format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
     }
 }
