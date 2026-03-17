99c60dfa38ec5c2ccce9bce0a8c5ca39f93d22f0
Juanelops <juan.gavira.pichaco@gmail.com>
2026-03-17 17:20:03 +0100

    Renaming parameter wr to wd in RegisterWorkDayUseCase

--- src/main/java/dev/jugapi/workcount/application/port/in/RegisterWorkDayUseCase.java
+++ src/main/java/dev/jugapi/workcount/application/port/in/RegisterWorkDayUseCase.java
@@ -3,5 +3,5 @@ package dev.jugapi.workcount.application.port.in;
 import dev.jugapi.workcount.domain.model.WorkDay;
 
 public interface RegisterWorkDayUseCase {
-    WorkDay registerWorkDay(WorkDay wr);
+    WorkDay registerWorkDay(WorkDay wd);
 }
