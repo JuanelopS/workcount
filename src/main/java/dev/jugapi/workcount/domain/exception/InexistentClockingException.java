dabdab5c86e2f7907064f3a5592ce4d6c221c1f2
Juanelops <juan.gavira.pichaco@gmail.com>
2026-04-18 12:48:05 +0200

    chore: improve error message in InexistentClockingException to include 'horas'

--- src/main/java/dev/jugapi/workcount/domain/exception/InexistentClockingException.java
+++ src/main/java/dev/jugapi/workcount/domain/exception/InexistentClockingException.java
@@ -1,12 +1,11 @@
 package dev.jugapi.workcount.domain.exception;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class InexistentClockingException extends RuntimeException {
     public InexistentClockingException(LocalTime time) {
-        super("No existen ningún fichaje a las  " +
-                time.format(DateTimeFormatter.ofPattern("HH:mm")));
+        super("No existe ningún fichaje a las  " +
+                time.format(DateTimeFormatter.ofPattern("HH:mm")) + " horas");
     }
 }
